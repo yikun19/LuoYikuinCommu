3efcffb05a6da83674cd85f1d8bf68a0646ad8f4
Y3ko <luoyikun19@19.com>
2022-07-10 14:48:48 +0800

    init

---
+++ src/main/java/com/Luoyikun/community/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.Luoyikun.community.controller;
+
+import com.Luoyikun.community.entity.DiscussPost;
+import com.Luoyikun.community.entity.Page;
+import com.Luoyikun.community.entity.User;
+import com.Luoyikun.community.service.DiscussPostService;
+import com.Luoyikun.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+
+        page.setRows(discussPostService.findDiscussPostRows(138));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(138, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if(list != null) {
+            for(DiscussPost discussPost : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", discussPost);
+                User user = userService.findUserById(discussPost.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts", discussPosts);
+        return "/index";
+    }
+}
---
+++ src/main/java/com/Luoyikun/community/dao/DiscussPostMapper.java
@@ -0,0 +1,15 @@
+package com.Luoyikun.community.dao;
+
+import com.Luoyikun.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    int selectDiscussPostRows(@Param("userId") int userId);
+}
